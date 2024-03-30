59232044845d733eb455e86ce47290447bfc9f08
ValtierraXavier <xavier.valtierra@icloud.com>
2024-03-30 15:38:20 -0400

    added router

---
+++ web/src/AddProjects/AddProject.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function addProject() {
+  return (
+    <div>
+        <h1>Add Project Page</h1>
+        <form name="addProject">
+            <input type='text' id='projectName'/>
+        </form>
+    </div>
+  )
+}
--- web/src/App.js
+++ web/src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import AddProject from './AddProjects/AddProject.jsx';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
            <h1>Home Page</h1> 
+           <Routes>
+            <Route path = '/addproject' element = {<AddProject/>}/>
+          </Routes>
     </div>
   );
 }
