245dc35624b2eee1885307cbd7b0c3bb4c9cda9d
vpol19 <88061349+vpol19@users.noreply.github.com>
2024-03-30 16:36:17 -0400

    rendered home page into app js

--- web/src/HomePage.js
+++ web/src/HomePage.js
@@ -6,8 +6,16 @@ export default function HomePage() {
   return (
     <div>
       <h1>Welcome to weWorkin! Start your journey below</h1>
-      <h2>I need dev hours on a project:</h2>
+      <h2>I need dev hours on a project</h2>
+      <p>
+        You’re a project lead! You should sign up here to read expectations for
+        a project lead and log your project{" "}
+      </p>
       <h2>I want to contribute code to a project!</h2>
+      <p>
+        Great. You're an apprentice. You should search our projects to match
+        your needs
+      </p>
       {/* <nav>
         <ul>
           <li>
