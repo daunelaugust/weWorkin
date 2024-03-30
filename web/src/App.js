a1c0ea08e3dddcf4bcb5b3acacb4cef94becf772
JuanHundred <juanquezada336@gmail.com>
2024-03-30 16:56:17 -0400

    Add Login page

--- web/src/App.js
+++ web/src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
 import AddProject from './AddProjects/AddProject.jsx';
+import LoginPage from './pages/LoginPage.jsx';
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-           <h1>Home Page</h1> 
-           <Routes>
-            <Route path = '/addproject' element = {<AddProject/>}/>
-          </Routes>
+      <h1>Home Page</h1> 
+      <Routes>
+        <Route path = '/addproject' element = {<AddProject/>}/>
+        <Route path = '/loginpage' element = {<LoginPage/>}/>
+      </Routes>
     </div>
   );
 }
--- web/src/index.js
+++ web/src/index.js
@@ -7,7 +7,8 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import {Auth0Provider} from '@auth0/auth0-react'
 
 
-
+const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
